428794e9b6974d42b8a611950a764708d8c1c873
nekitamolik <135228429+nekitamolik@users.noreply.github.com>
2023-06-01 16:45:00 +0200

    Add files via upload

---
+++ B9/Matura_Zadatak_B9/Matura_Zadatak_B9/Katalog.aspx
@@ -0,0 +1,95 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Katalog.aspx.cs" Inherits="Matura_Zadatak_B9._Default" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>CD Katalog</title>
+    <link rel="Stylesheet" href="CSS/StyleSheet.css" />
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div class="header">
+            <ul>
+                <li class="big">CD K
+                </li>
+                <li class="small">ATALOG
+                </li>
+            </ul>
+        </div>
+        <div class="menu">
+            <ul>
+                <li><a href="Katalog.aspx">Katalog</a></li>
+                <li><a href="HTML Pages/Uputstvo.html">Uputstvo</a></li>
+            </ul>
+        </div>
+        <div class="content">
+            <div class="form">
+                <table>
+                    <tr>
+                        <td>
+                            <label>Izvođač:</label>
+                        </td>
+                        <td>
+                            <asp:TextBox ID="TextBoxIzvodjac" runat="server" Width="200px"></asp:TextBox>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <label>Naziv:</label>
+                        </td>
+                        <td>
+                            <asp:TextBox ID="TextBoxNaziv" runat="server" Width="200px"></asp:TextBox>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <label>Žanr:</label>
+                        </td>
+                        <td>
+                            <asp:DropDownList ID="DropDownListZanr" runat="server" Width="120px" ></asp:DropDownList>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <label>Godina izdanja:</label>
+                        </td>
+                        <td>
+                            <asp:DropDownList ID="DropDownListGodina" runat="server" Width="70px" ></asp:DropDownList>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <label>Izdavačka kuća:</label>
+                        </td>
+                        <td>
+                            <asp:TextBox ID="TextBoxIzdavackaKuca" runat="server" Width="200px"></asp:TextBox>
+
+                        </td>
+                    </tr>
+                </table>
+            </div>
+            <div class="tableshow">
+                <asp:GridView ID="GV" runat="server" OnRowDataBound="gv_RowDataBound" AutoGenerateColumns="False">
+                    <Columns>
+                        <asp:BoundField DataField="izvodjac" HeaderText="Izvođač" />
+                        <asp:BoundField DataField="naziv" HeaderText="Naziv" />
+                         <asp:BoundField DataField="zanr" HeaderText="Žanr" />
+                        <asp:BoundField DataField="godina" HeaderText="Godina" />
+                         <asp:BoundField DataField="izdavacka kuca" HeaderText="Izdavačka kuća" />
+                        <asp:TemplateField HeaderText="Slika omota" >
+                            <ItemTemplate><asp:Image ID="slika" runat="server"></asp:Image></ItemTemplate>
+
+                        </asp:TemplateField>
+                    </Columns>
+                </asp:GridView>
+                
+            </div>
+            <asp:Button ID="src" runat="server" Text="Trazi" OnClick="src_Click" />
+        </div>
+        <div class="footer">
+            <p>© Klub kolekcionar</p>
+        </div>
+    </form>
+</body>
+</html>
---
+++ B9/Matura_Zadatak_B9/Matura_Zadatak_B9/Katalog.aspx.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.IO;
+
+namespace Matura_Zadatak_B9
+{
+    public partial class _Default : System.Web.UI.Page
+    {
+        DataTable t = new DataTable();
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+                t.Columns.Add("Izvodjac", typeof(string));
+                t.Columns.Add("Naziv", typeof(string));
+                t.Columns.Add("Zanr", typeof(string));
+                t.Columns.Add("Godina", typeof(string));
+                t.Columns.Add("Izdavacka kuca", typeof(string));
+                t.Columns.Add("Slika omota", typeof(string));
+
+                string[] rows = File.ReadAllLines(Server.MapPath(@"Source\Katalog.txt"));
+                foreach (string x in rows)
+                {
+                    string[] cells = x.Split(new char[] { ' ', '|' }, StringSplitOptions.RemoveEmptyEntries);
+                    DataRow row = t.NewRow();
+                    row[0] = cells[0];
+                    row[1] = cells[1];
+                    row[2] = cells[2];
+                    row[3] = cells[3];
+                    row[4] = cells[4];
+                    row[5] = cells[5];
+                    t.Rows.Add(row);
+                }
+
+                List<string> god = new List<string>();
+                List<string> zanr = new List<string>();
+                foreach (DataRow x in t.Rows)
+                {
+                    if (!god.Contains(x[3].ToString()))
+                        god.Add(x[3].ToString());
+                    if (!zanr.Contains(x[2].ToString()))
+                        zanr.Add(x[2].ToString());
+                }
+                god.Sort();
+                zanr.Sort();
+          if (!IsPostBack)
+                {
+                DropDownListGodina.Items.Clear();
+                DropDownListZanr.Items.Clear();
+                DropDownListZanr.Items.Add("");
+                DropDownListGodina.Items.Add("");
+                foreach (string x in god)
+                    DropDownListGodina.Items.Add(x);
+                foreach (string x in zanr)
+                    DropDownListZanr.Items.Add(x);
+            }
+        }
+
+        protected void gv_RowDataBound(object sender, GridViewRowEventArgs e)
+        {
+            if (e.Row.RowType == DataControlRowType.DataRow)
+            {
+                (e.Row.FindControl("slika") as Image).ImageUrl = (e.Row.DataItem as DataRowView)[5].ToString();
+            }
+        }
+
+        private DataTable GetTable(string izvodjac, string naziv, string zanr, string godina, string izdavacka_kuca)
+        {
+            bool match = true;
+
+            DataTable t2 = new DataTable();
+            t2.Columns.Add("Izvodjac", typeof(string));
+            t2.Columns.Add("Naziv", typeof(string));
+            t2.Columns.Add("Zanr", typeof(string));
+            t2.Columns.Add("Godina", typeof(string));
+            t2.Columns.Add("Izdavacka kuca", typeof(string));
+            t2.Columns.Add("Slika omota", typeof(string));
+
+            foreach (DataRow x in t.Rows)
+            {
+                match = true;
+                if (!string.IsNullOrEmpty(izvodjac))
+                {
+                    if (!(x[0].ToString().ToLower().StartsWith(izvodjac.ToLower())))
+                    {
+
+                        match = false;
+                    }
+                }
+                if (!string.IsNullOrEmpty(naziv))
+                {
+                    if (!(x[1].ToString().ToLower().StartsWith(naziv.ToLower())))
+                    {
+                        match = false;
+                    }
+                }
+
+                if (!string.IsNullOrEmpty(zanr))
+                {
+                    if (!(x[2].ToString().ToLower() == zanr.ToLower()))
+                    {
+                        match = false;
+                    }
+                }
+                if (!string.IsNullOrEmpty(godina))
+                {
+                    if (!(x[3].ToString().ToLower() == godina.ToLower()))
+                    {
+                        match = false;
+                    }
+                }
+                if (!string.IsNullOrEmpty(izdavacka_kuca))
+                {
+                    if (!(x[4].ToString().ToLower().StartsWith(izdavacka_kuca.ToLower())))
+                    {
+                        match = false;
+                    }
+                }
+
+                if (match)
+                    t2.ImportRow(x);
+            }
+
+            return t2;
+        }
+
+        protected void src_Click(object sender, EventArgs e)
+        {
+            GV.DataSource = null;
+            DataTable show = GetTable(TextBoxIzvodjac.Text, TextBoxNaziv.Text, DropDownListZanr.SelectedValue.ToString(), DropDownListGodina.SelectedValue.ToString(), TextBoxIzdavackaKuca.Text);
+            GV.DataSource = show;
+            GV.DataBind();
+        }
+    }
+}
