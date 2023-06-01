807475bbaa9b5b2a4a6f3d0314331add51d233a5
nekitamolik <135228429+nekitamolik@users.noreply.github.com>
2023-06-01 16:51:53 +0200

    Add files via upload

---
+++ B13/Matura_Zadatak_B13/Matura_Zadatak_B13/Pocetna.aspx
@@ -0,0 +1,93 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Pocetna.aspx.cs" Inherits="Matura_Zadatak_B13._Default" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Telefonski imenik</title>
+    <link rel="Stylesheet" href="CSS/StyleSheet.css" />
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div class="header">
+            <ul>
+                <li>Zadatak 6
+                </li>
+                <li>Telefonski imenik
+                </li>
+            </ul>
+        </div>
+        <div class="content">
+            <div class="form">
+                <table>
+                    <tr>
+                        <td colspan="7" class="headlabel">Parametri za pretragu
+                        </td>
+                    </tr>
+                    <tr>
+                        <td class="label">Ime:
+                        </td>
+                        <td class="textbox">
+                            <asp:TextBox ID="TextBoxIme" runat="server" Width="200px"></asp:TextBox>
+                        </td>
+                        <td class="label">Adresa:
+                        </td>
+                        <td class="textbox">
+                            <asp:TextBox ID="TextBoxAdresa" runat="server" Width="200px"></asp:TextBox>
+                        </td>
+                        <td class="label">Telefon:
+                        </td>
+                        <td class="textbox">
+                            <asp:TextBox ID="TextBoxTel" runat="server" Width="200px"></asp:TextBox>
+                        </td>
+                        <td rowspan="2" class="button">
+                            <asp:Button ID="ButtonSrc" runat="server" Text="Trazi" OnClick="ButtonSrc_Click" Width="80px" Height="25px" />
+                        </td>
+                    </tr>
+                    <tr>
+                        <td class="label">Prezime:
+                        </td>
+                        <td class="textbox">
+                            <asp:TextBox ID="TextBoxPrezime" runat="server" Width="200px"></asp:TextBox>
+                        </td>
+                        <td class="label">Mesto:
+                        </td>
+                        <td class="textbox">
+                            <asp:DropDownList ID="DropDownListMesto" runat="server" Width="80px"></asp:DropDownList>
+                        </td>
+                        <td class="label">Email:
+                        </td>
+                        <td class="textbox">
+                            <asp:TextBox ID="TextBoxEmail" runat="server" Width="200px"></asp:TextBox>
+                        </td>
+                    </tr>
+                </table>
+            </div>
+            <hr />
+            <div class="GV">
+                <asp:GridView ID="GV" runat="server"></asp:GridView>
+            </div>
+        </div>
+        <div class="footer">
+            <table>
+            <tr>
+                <td class="copyright">Elektrotehnicka skola "Nikola Tesla" Nis
+                </td>
+                <td class="menu">
+                    <a href="HTML Pages/Vazni Telefoni.html">Vazni Telefoni
+                    </a>
+                </td>
+                <td class="menu">
+                    <a href="HTML Pages/Uputstvo.html">Uputstvo
+                    </a>
+                </td>
+                <td class="menu">
+                    <a href="HTML Pages/O autoru.html">O autoru
+                    </a>
+                </td>
+            </tr>
+                </table>
+        </div>
+    </form>
+</body>
+</html>
---
+++ B13/Matura_Zadatak_B13/Matura_Zadatak_B13/Pocetna.aspx.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.IO;
+using System.Data;
+
+namespace Matura_Zadatak_B13
+{
+    public partial class _Default : System.Web.UI.Page
+    {
+
+        DataTable imenik = new DataTable();
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            imenik.Columns.Add("Redni broj", typeof(string));
+            imenik.Columns.Add("Ime", typeof(string));
+            imenik.Columns.Add("Prezime", typeof(string));
+            imenik.Columns.Add("Adresa", typeof(string));
+            imenik.Columns.Add("Broj Telefona", typeof(string));
+            imenik.Columns.Add("Email", typeof(string));
+            imenik.Columns.Add("Mesto", typeof(string));
+
+            string[] lines = File.ReadAllLines(Server.MapPath(@"\Source\imenik.txt"));
+            foreach (string x in lines) 
+            {
+                string[] split = x.Split(new string[] { " | " }, StringSplitOptions.RemoveEmptyEntries);
+                DataRow row = imenik.NewRow();
+                row[0] = split[0] + ".";
+                row[1] = split[1];
+                row[2] = split[2];
+                row[3] = split[3];
+                row[4] = split[4];
+                row[5] = split[5];
+                row[6] = split[6];
+                imenik.Rows.Add(row);
+            }
+
+            DropDownListMesto.Items.Add("");
+            foreach (DataRow x in imenik.Rows) 
+                if (!(DropDownListMesto.Items.Contains(new ListItem(x[6].ToString()))))
+                    DropDownListMesto.Items.Add(x[6].ToString());
+
+            GV.DataSource = imenik;
+            GV.DataBind();
+        }
+
+        protected void ButtonSrc_Click(object sender, EventArgs e)
+        {
+            GV.DataSource = null;
+            DataTable src = new DataTable();
+            src.Columns.Add("Redni broj", typeof(string));
+            src.Columns.Add("Ime", typeof(string));
+            src.Columns.Add("Prezime", typeof(string));
+            src.Columns.Add("Adresa", typeof(string));
+            src.Columns.Add("Broj Telefona", typeof(string));
+            src.Columns.Add("Email", typeof(string));
+            src.Columns.Add("Mesto", typeof(string));
+
+            bool match;
+            foreach (DataRow x in imenik.Rows) 
+            {
+                match = true;
+                if (!string.IsNullOrEmpty(TextBoxIme.Text))
+                    if (!(x[1].ToString().ToLower().StartsWith(TextBoxIme.Text.ToLower())))
+                        match = false;
+                if (!string.IsNullOrEmpty(TextBoxPrezime.Text))
+                    if (!(x[2].ToString().ToLower().StartsWith(TextBoxPrezime.Text.ToLower())))
+                        match = false;
+                if (!string.IsNullOrEmpty(TextBoxAdresa.Text))
+                    if (!(x[3].ToString().ToLower().StartsWith(TextBoxAdresa.Text.ToLower())))
+                        match = false;
+                if (!string.IsNullOrEmpty(TextBoxTel.Text))
+                    if (!(x[4].ToString().ToLower().StartsWith(TextBoxTel.Text.ToLower())))
+                        match = false;
+                if (!string.IsNullOrEmpty(TextBoxEmail.Text))
+                    if (!(x[5].ToString().ToLower().StartsWith(TextBoxEmail.Text.ToLower())))
+                        match = false;
+                if (!string.IsNullOrEmpty(DropDownListMesto.SelectedValue.ToString()))
+                    if (!(x[6].ToString().ToLower() == DropDownListMesto.SelectedValue.ToString().ToLower()))
+                        match = false;
+                if (match) 
+                {
+                    DataRow pom = src.NewRow();
+                    pom[0] = x[0];
+                    pom[1] = x[1];
+                    pom[2] = x[2];
+                    pom[3] = x[3];
+                    pom[4] = x[4];
+                    pom[5] = x[5];
+                    pom[6] = x[6];
+                    src.Rows.Add(pom);
+                }
+            }
+            GV.DataSource = src;
+            GV.DataBind();
+        }
+    }
+}
