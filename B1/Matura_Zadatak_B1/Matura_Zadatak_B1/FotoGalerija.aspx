2192aa09231c165f8ad2c81f49ed42e3fc56fae2
nekitamolik <135228429+nekitamolik@users.noreply.github.com>
2023-06-01 16:34:54 +0200

    Add files via upload

---
+++ B1/Matura_Zadatak_B1/Matura_Zadatak_B1/FotoGalerija.aspx
@@ -0,0 +1,42 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="FotoGalerija.aspx.cs" Inherits="Matura_Zadatak_B1._Default" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head id="Head1" runat="server">
+    <title>Foto Galerija</title>
+    <link rel="stylesheet" href="../CSS/StyleSheet.css" />
+</head>
+<body>
+    <form id="forma" runat="server">
+
+        <h1>Фото галерија</h1>
+        <div class="menu">
+            <ul class="menulist">
+                <li id="btnStart"><a href="FotoGalerija.aspx">Почетна</a></li>
+                <li id="btnAbout"><a href="../HTML Pages/O autoru.html">О аутору</a></li>
+                <li id="btnManual"><a href="../HTML Pages/Uputstvo.html">Упутство</a></li>
+                <li id="lblExam">Завршни испит</li>
+            </ul>
+        </div>
+        <div class="gallery">
+            <textarea id="TextArea" runat="server"></textarea>
+            <div class="bigPicture">
+                <asp:Image ID="imgPrikaz" runat="server" Width="640" Height="480" />
+            </div>
+            <div class="imgButtons">
+                <asp:ImageButton ID="ImageButton1" runat="server" OnClick="ImageButton1_Click" />
+                <asp:ImageButton ID="ImageButton2" runat="server" OnClick="ImageButton2_Click" />
+                <asp:ImageButton ID="ImageButton3" runat="server" OnClick="ImageButton3_Click" />
+                <asp:ImageButton ID="ImageButton4" runat="server" OnClick="ImageButton4_Click" />
+                <asp:ImageButton ID="ImageButton5" runat="server" OnClick="ImageButton5_Click" />
+                <asp:ImageButton ID="ImageButton6" runat="server" OnClick="ImageButton6_Click" />
+                <asp:ImageButton ID="ImageButton7" runat="server" OnClick="ImageButton7_Click" />
+                <asp:ImageButton ID="ImageButton8" runat="server" OnClick="ImageButton8_Click" />
+                <asp:ImageButton ID="ImageButton9" runat="server" OnClick="ImageButton9_Click" />
+                <asp:ImageButton ID="ImageButton10" runat="server" OnClick="ImageButton10_Click" />
+            </div>
+        </div>
+    </form>
+</body>
+</html>
---
+++ B1/Matura_Zadatak_B1/Matura_Zadatak_B1/FotoGalerija.aspx.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Matura_Zadatak_B1
+{
+    public partial class _Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            imgPrikaz.ImageUrl = "/Images/slika1.jpg";
+            ImageButton1.ImageUrl = "/Images/slika1.jpg";
+            ImageButton2.ImageUrl = "/Images/slika2.jpg";
+            ImageButton3.ImageUrl = "/Images/slika3.jpg";
+            ImageButton4.ImageUrl = "/Images/slika4.jpg";
+            ImageButton5.ImageUrl = "/Images/slika5.jpg";
+            ImageButton6.ImageUrl = "/Images/slika6.jpg";
+            ImageButton7.ImageUrl = "/Images/slika7.jpg";
+            ImageButton8.ImageUrl = "/Images/slika8.jpg";
+            ImageButton9.ImageUrl = "/Images/slika9.jpg";
+            ImageButton10.ImageUrl = "/Images/slika10.jpg";
+
+            TextArea.InnerText = @"Ancient Egypt was a civilization in Northeast Africa situated in the Nile Valley.Ancient Egyptian civilization followed prehistoric Egypt and coalesced around 3100 BC (according to conventional Egyptian chronology)with the political unification of Upper and Lower Egypt under Menes (often identified with Narmer). The history of ancient Egypt occurred as a series of stable kingdoms, separated by periods of relative instability known as IntermediatePeriods: the Old Kingdom of the Early Bronze Age, the Middle Kingdom of the Middle Bronze Age and the New Kingdom of the Late Bronze Age.";
+        }
+
+        protected void ImageButton1_Click(object sender, ImageClickEventArgs e)
+        {
+            imgPrikaz.ImageUrl = "/Images/slika1.jpg";
+            TextArea.InnerText = @"Ancient Egypt was a civilization in Northeast Africa situated in the Nile Valley.Ancient Egyptian civilization followed prehistoric Egypt and coalesced around 3100 BC (according to conventional Egyptian chronology)with the political unification of Upper and Lower Egypt under Menes (often identified with Narmer).The history of ancient Egypt occurred as a series of stable kingdoms, separated by periods of relative instability known as IntermediatePeriods: the Old Kingdom of the Early Bronze Age, the Middle Kingdom of the Middle Bronze Age and the New Kingdom of the Late Bronze Age.";
+        }
+
+        protected void ImageButton2_Click(object sender, ImageClickEventArgs e)
+        {
+            imgPrikaz.ImageUrl = "/Images/slika2.jpg";
+            TextArea.InnerText = @"Egypt reached the pinnacle of its power in the New Kingdom, ruling much of Nubia and a sizable portion of the Near East, after which it entered a period of slow decline. During the course of its history,Egypt was invaded or conquered by a number of foreign powers, including the Hyksos, the Libyans, the Nubians, the Assyrians,the Achaemenid Persians, and the Macedonians under the command of Alexander the Great. The Greek Ptolemaic Kingdom, formed in the aftermath of Alexander's death, ruled Egypt until 30 BC, when, under Cleopatra, it fell to the Roman Empire and became a Roman province.";
+        }
+
+        protected void ImageButton3_Click(object sender, ImageClickEventArgs e)
+        {
+            imgPrikaz.ImageUrl = "/Images/slika3.jpg";
+            TextArea.InnerText = @"The success of ancient Egyptian civilization came partly from its ability to adapt to the conditions of the Nile River valley for agriculture. The predictable flooding and controlled irrigation of the fertile valley produced surplus crops, which supported a more dense population, and social development and culture. With resources to spare, the administration sponsored mineralexploitation of the valley and surrounding desert regions, the early development of an independent writing system, the organization of collective construction and agricultural projects, trade with surrounding regions, and a military intended to assert Egyptian dominance.";
+        }
+
+        protected void ImageButton4_Click(object sender, ImageClickEventArgs e)
+        {
+            imgPrikaz.ImageUrl = "/Images/slika4.jpg";
+            TextArea.InnerText = @"Motivating and organizing these activities was a bureaucracy of elite scribes,religious leaders, and administrators under the control of a pharaoh,who ensured the cooperation and unity of the Egyptian people in the context of an elaborate system of religious beliefs.";
+        }
+
+        protected void ImageButton5_Click(object sender, ImageClickEventArgs e)
+        {
+            imgPrikaz.ImageUrl = "/Images/slika5.jpg";
+            TextArea.InnerText = @"The many achievements of the ancient Egyptians include the quarrying, surveying,and construction techniques that supported the building of monumental pyramids, temples, and obelisks; a system of mathematics, a practical and effective system of medicine, irrigation systems, and agricultural production techniques, the first known planked boats,[5] Egyptian faience and glass technology, new forms of literature, and the earliest known peace treaty, made with the Hittites.";
+        }
+
+        protected void ImageButton6_Click(object sender, ImageClickEventArgs e)
+        {
+            imgPrikaz.ImageUrl = "/Images/slika6.jpg";
+            TextArea.InnerText = @"Ancient Egypt has left a lasting legacy. Its art and architecture were widely copied, and its antiquities were carried off to far corners of the world. Its monumental ruins have inspired the imaginations of travelers and writers for millennia. A newfound respect for antiquities and excavations in the early modern period by Europeans andEgyptians has led to the scientific investigation of Egyptian civilization and a greater appreciation of its cultural legacy.";
+        }
+
+        protected void ImageButton7_Click(object sender, ImageClickEventArgs e)
+        {
+            imgPrikaz.ImageUrl = "/Images/slika7.jpg";
+            TextArea.InnerText = @"The Nile has been the lifeline of its region for much of human history.[8] The fertile floodplain of the Nile gave humans the opportunity to develop a settled agricultural economy and a moresophisticated, centralized society that became a cornerstone in the history of human civilization.[9] Nomadic modernhuman hunter-gatherers began living in the Nile valley through the end of the Middle Pleistocene some 120,000 years ago.By the late Paleolithic period, the arid climate of Northern Africa had become increasingly hot and dry, forcing the populationsof the area to concentrate along the river region.";
+        }
+
+        protected void ImageButton8_Click(object sender, ImageClickEventArgs e)
+        {
+            imgPrikaz.ImageUrl = "/Images/slika8.jpg";
+            TextArea.InnerText = @"The Early Dynastic Period was approximately contemporary to the early Sumerian-Akkadian civilization  of Mesopotamia and of ancient Elam. The third-century BC Egyptian priest Manetho grouped the long line of kings from Menes to his own timeinto 30 dynasties, a system still used today. He began his official history with the king named (or Menes in Greek), who was believed to haveunited the two kingdoms of Upper and Lower Egypt";
+        }
+
+        protected void ImageButton9_Click(object sender, ImageClickEventArgs e)
+        {
+            imgPrikaz.ImageUrl = "/Images/slika9.jpg";
+            TextArea.InnerText = @"The many achievements of the ancient Egyptians include the quarrying, surveying,and construction techniques that supported the building of monumental pyramids, temples, and obelisks; a system of mathematics, a practical and effective system of medicine, irrigation systems, and agricultural production techniques, the first known planked boats,[5] Egyptian faience and glass technology, new forms of literature, and the earliest known peace treaty, made with the Hittites.";
+        }
+
+        protected void ImageButton10_Click(object sender, ImageClickEventArgs e)
+        {
+            imgPrikaz.ImageUrl = "/Images/slika10.jpg";
+            TextArea.InnerText = @"Egypt reached the pinnacle of its power in the New Kingdom, ruling much of Nubia and a sizable portion of the Near East, after which it entered a period of slow decline. During the course of its history,Egypt was invaded or conquered by a number of foreign powers, including the Hyksos, the Libyans, the Nubians, the Assyrians,the Achaemenid Persians, and the Macedonians under the command of Alexander the Great. The Greek Ptolemaic Kingdom, formed in the aftermath of Alexander's death, ruled Egypt until 30 BC, when, under Cleopatra, it fell to the Roman Empire and became a Roman province.";
+        }
+    }
+}
