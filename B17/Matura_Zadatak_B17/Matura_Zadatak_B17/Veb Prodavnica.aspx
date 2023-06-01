3c977275008524e426357a6acc96c3a25e5765a1
nekitamolik <135228429+nekitamolik@users.noreply.github.com>
2023-06-01 16:59:11 +0200

    Add files via upload

---
+++ B17/Matura_Zadatak_B17/Matura_Zadatak_B17/Veb Prodavnica.aspx
@@ -0,0 +1,125 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Veb Prodavnica.aspx.cs" Inherits="Matura_Zadatak_B17._Default" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Veb Prodavnica</title>
+<link rel="Stylesheet" href="CSS/StyleSheet.css" />
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div class="header">
+            <ul>
+                <li>Zadatak 14</li>
+                <li>TV Program</li>
+            </ul>
+        </div>
+        <div class="content">
+            <div class="headertext">
+                <img id="image" src="Images/Icon.jpg" alt="slika" width="200px" height="150px" />
+                    <h2>Web Prodavnica</h2>
+            </div>
+            <br />
+            <hr />
+            <br />
+            <div class="search">
+                <label class="label">Parametri za pretragu:</label>
+                <br />
+                <br />
+                <table id="src">
+                    <tr>
+                        <td>
+                            <label class="label">Proizvodjac:</label>
+                        </td>
+                        <td>
+                            <asp:DropDownList ID="DropDownListProizvodjac" runat="server" Width="100px"></asp:DropDownList>
+                        </td>
+                        <td>
+                            <label class="label">Kamera:</label>
+                        </td>
+                        <td>
+                            <asp:DropDownList ID="DropDownListKamera" runat="server" Width="100px"></asp:DropDownList>
+                        </td>
+                        <td>
+                            <label class="label">Ekran:</label>
+                        </td>
+                        <td>
+                            <asp:DropDownList ID="DropDownListEkran" runat="server" Width="100px"></asp:DropDownList>
+                        </td>
+                        <td rowspan="2">
+                            <asp:Button ID="ButtonSrc" runat="server" Text="Pronadji" Width="80px" OnClick="ButtonSrc_Click"/>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <label class="label">RAM:</label>
+                        </td>
+                        <td>
+                            <asp:DropDownList ID="DropDownListRAM" runat="server" Width="100px"></asp:DropDownList>
+                        </td>
+                        <td>
+                            <label class="label">Dual SIM:</label>
+                        </td>
+                        <td>
+                            <asp:DropDownList ID="DropDownListSIM" runat="server" Width="100px"></asp:DropDownList>
+                        </td>
+                        <td>
+                            <label class="label">Procesor:</label>
+                        </td>
+                        <td>
+                            <asp:DropDownList ID="DropDownListProcesor" runat="server" Width="100px"></asp:DropDownList>
+                        </td>
+                        </tr>
+                </table>
+            </div>
+            <br />
+            <hr />
+            <br />
+            <div class="GV">
+                <asp:GridView ID="GV" runat="server" AutoGenerateColumns="False" OnRowDataBound="GV_RowDataBound" >
+                    <Columns>
+                        <asp:TemplateField HeaderText="" HeaderStyle-Width="250px">
+                            <ItemTemplate>
+                                <asp:Image ID="slika" runat="server" />
+                            </ItemTemplate>
+                        </asp:TemplateField>
+                        <asp:TemplateField HeaderText="Karakteristike">
+                            <ItemTemplate>
+                                <br />
+                                <br />
+                                <asp:Label ID="naziv" CssClass="name" runat="server" ></asp:Label>
+                                <br />
+                                <br />
+                                <asp:Label ID="proizvodjac" CssClass="stats" runat="server" ></asp:Label>
+                                <br />
+                                <asp:Label ID="RAM" CssClass="stats" runat="server" ></asp:Label>
+                                <br />
+                                <asp:Label ID="procesor" CssClass="stats" runat="server" ></asp:Label>
+                                <br />
+                                <asp:Label ID="ekran" CssClass="stats" runat="server" ></asp:Label>
+                                <br />
+                                <asp:Label ID="kamera" CssClass="stats" runat="server" ></asp:Label>
+                                <br />
+                            </ItemTemplate>
+                        </asp:TemplateField>
+                        <asp:BoundField DataField="Cena" ItemStyle-CssClass="cena" HeaderText="Cena" HeaderStyle-Width="10%" />
+                        
+                    </Columns>
+                </asp:GridView>
+            </div>
+        </div>
+        <div class="footer">
+            <table>
+                <tr>
+                    <td>Elektrotehnicka skola "Nikola Tesla" Nis
+                    </td>
+                    <td>
+                        <a href="HTML Pages/Uputstvo.html">Uputstvo</a>
+                    </td>
+                </tr>
+            </table>
+        </div>
+    </form>
+</body>
+</html>
---
+++ B17/Matura_Zadatak_B17/Matura_Zadatak_B17/Veb Prodavnica.aspx.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.IO;
+
+namespace Matura_Zadatak_B17
+{
+    public partial class _Default : System.Web.UI.Page
+    {
+        DataTable phones = new DataTable();
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+            phones.Columns.Add("sifra", typeof(string));
+            phones.Columns.Add("naziv artikla", typeof(string));
+            phones.Columns.Add("proizvodjac", typeof(string));
+            phones.Columns.Add("RAM memorija", typeof(string));
+            phones.Columns.Add("tip procesora", typeof(string));
+            phones.Columns.Add("kamera", typeof(string));
+            phones.Columns.Add("ekran", typeof(string));
+            phones.Columns.Add("slika", typeof(string));
+            phones.Columns.Add("cena", typeof(string));
+
+            string[] lines = File.ReadAllLines(Server.MapPath("/TextFiles/vebprodavnica.txt"));
+            foreach (string x in lines)
+            {
+                DataRow row = phones.NewRow();
+                row[0] = x.Substring(0, 5).Trim();
+                row[1] = x.Substring(5, 25).Trim();
+                row[2] = x.Substring(30, 20).Trim();
+                row[3] = x.Substring(50, 5).Trim();
+                row[4] = x.Substring(55, 15).Trim();
+                row[5] = x.Substring(70, 10).Trim();
+                row[6] = x.Substring(80, 10).Trim();
+                row[7] = x.Substring(90, 30).Trim();
+                row[8] = x.Substring(120, 10).Trim();
+                phones.Rows.Add(row);
+            }
+
+            if (!IsPostBack)
+            {
+                DropDownListProizvodjac.Items.Add("");
+                DropDownListEkran.Items.Add("");
+                DropDownListKamera.Items.Add("");
+                DropDownListProcesor.Items.Add("");
+                DropDownListRAM.Items.Add("");
+                DropDownListSIM.Items.Add("");
+
+                DropDownListRAM.Items.Add("512MB");
+                DropDownListRAM.Items.Add("1GB");
+                DropDownListRAM.Items.Add("1.5GB");
+                DropDownListRAM.Items.Add("2GB");
+                DropDownListRAM.Items.Add("3GB");
+
+                foreach (DataRow x in phones.Rows)
+                {
+                    if (!DropDownListProizvodjac.Items.Contains(new ListItem(x[2].ToString())))
+                        DropDownListProizvodjac.Items.Add(x[2].ToString());
+                    if (!DropDownListKamera.Items.Contains(new ListItem(x[5].ToString())))
+                        DropDownListKamera.Items.Add(x[5].ToString());
+                    if (!DropDownListEkran.Items.Contains(new ListItem(x[6].ToString())))
+                        DropDownListEkran.Items.Add(x[6].ToString());
+                    if (!DropDownListProcesor.Items.Contains(new ListItem(x[4].ToString())))
+                        DropDownListProcesor.Items.Add(x[4].ToString());
+                }
+            }
+        }
+
+        protected void ButtonSrc_Click(object sender, EventArgs e)
+        {
+            GV.DataSource = null;
+            bool match = false;
+            DataTable t = new DataTable();
+            t.Columns.Add("slika",typeof(string));
+            t.Columns.Add("karakteristike",typeof(string));
+            t.Columns.Add("cena",typeof(string));
+
+            foreach (DataRow x in phones.Rows) 
+            {
+                match = true;
+                if (!string.IsNullOrEmpty(DropDownListProizvodjac.SelectedValue))
+                    if (!(DropDownListProizvodjac.SelectedValue == x[2].ToString())) 
+                    {
+                        match = false;
+                        continue;
+                    }
+                if (!string.IsNullOrEmpty(DropDownListKamera.SelectedValue))
+                    if (!(DropDownListKamera.SelectedValue == x[5].ToString()))
+                    {
+                        match = false;
+                        continue;
+                    }
+                if (!string.IsNullOrEmpty(DropDownListEkran.SelectedValue))
+                    if (!(DropDownListEkran.SelectedValue == x[6].ToString()))
+                    {
+                        match = false;
+                        continue;
+                    }
+                if (!string.IsNullOrEmpty(DropDownListRAM.SelectedValue))
+                    if (!(DropDownListRAM.SelectedValue == x[3].ToString()))
+                    {
+                        match = false;
+                        continue;
+                    }
+                if (!string.IsNullOrEmpty(DropDownListProcesor.SelectedValue))
+                    if (!(DropDownListProcesor.SelectedValue == x[4].ToString()))
+                    {
+                        match = false;
+                        continue;
+                    }
+                if (match) 
+                {
+                    DataRow row = t.NewRow();
+                    row[0] = x[7];
+                    row[1] = x[1] + "|Proizvodjac: " + x[2].ToString() + "|RAM: " + x[3].ToString() + "|Procesor: " + x[4].ToString() + " Core|Ekran: " + x[6].ToString() + "|Kamera: " + x[5].ToString();
+                    row[2] = x[8];
+                    t.Rows.Add(row);
+                }
+            }
+            GV.DataSource = t;
+            GV.DataBind();
+        }
+
+        protected void GV_RowDataBound(object sender, GridViewRowEventArgs e)
+        {
+            if (e.Row.RowType == DataControlRowType.DataRow)
+            {
+                string[] stats = (e.Row.DataItem as DataRowView)[1].ToString().Split('|');
+                (e.Row.FindControl("naziv") as Label).Text = stats[0];
+                (e.Row.FindControl("proizvodjac") as Label).Text = stats[1];
+                (e.Row.FindControl("RAM") as Label).Text = stats[2];
+                (e.Row.FindControl("procesor") as Label).Text = stats[3];
+                (e.Row.FindControl("ekran") as Label).Text = stats[4];
+                (e.Row.FindControl("kamera") as Label).Text = stats[5]; 
+                (e.Row.FindControl("slika") as Image).ImageUrl = (e.Row.DataItem as DataRowView)[0].ToString();
+            }
+        }
+    }
+}
