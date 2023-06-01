3b7de4ce55f7201396ea1c285c9527f1edded4f3
nekitamolik <135228429+nekitamolik@users.noreply.github.com>
2023-06-01 16:55:45 +0200

    Add files via upload

---
+++ B15/Matura_Zadatak_B15/Matura_Zadatak_B15/Red Voznje.aspx
@@ -0,0 +1,68 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Red Voznje.aspx.cs" Inherits="Matura_Zadatak_B15._Default" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Red Voznje</title>
+    <link rel="Stylesheet" href="CSS/StyleSheet.css" />
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div class="header">
+            <ul>
+                <li>Zadatak 14</li>
+                <li>TV Program</li>
+            </ul>
+        </div>
+        <div class="content">
+            <div class="search">
+                <table>
+                    <tr>
+                        <td>
+                            <label class="label">Linija:</label>
+                        </td>
+                        <td>
+                            <asp:DropDownList ID="DropDownListLinija" runat="server" Width="100px" AutoPostBack="true" OnSelectedIndexChanged="DropDownListLinija_SelectedIndexChanged"></asp:DropDownList>
+                        </td>
+                        <td>
+                            <label class="label">Smer:</label>
+                        </td>
+                        <td>
+                            <asp:DropDownList ID="DropDownListSmer" runat="server" Width="200px"></asp:DropDownList>
+                        </td>
+                        <td>
+                            <asp:Button ID="ButtonSrc" runat="server" Text="Pronadji" Width="80px" OnClick="ButtonSrc_Click"/>
+                        </td>
+                    </tr>
+                </table>
+            </div>
+            <div class="GV">
+                <label class="label">Polasci:</label>
+                <br />
+                <asp:GridView ID="GV" runat="server" AutoGenerateColumns="False" >
+                    <Columns>
+                        <asp:BoundField DataField="Rbr" HeaderText="Rbr" />
+                        <asp:BoundField DataField="Polazak" HeaderText="Polazak" />
+                        
+                    </Columns>
+                </asp:GridView>
+            </div>
+        </div>
+        <div class="footer">
+            <table>
+                <tr>
+                    <td>Elektrotehnicka skola "Nikola Tesla" Nis
+                    </td>
+                    <td>
+                        <a href="HTML Pages/Uputstvo.html">Uputstvo</a>
+                    </td>
+                    <td>
+                        <a href="HTML Pages/Kontakt.html">Kontakt</a>
+                    </td>
+                </tr>
+            </table>
+        </div>
+    </form>
+</body>
+</html>
---
+++ B15/Matura_Zadatak_B15/Matura_Zadatak_B15/Red Voznje.aspx.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.IO;
+using System.Data;
+
+namespace Matura_Zadatak_B15
+{
+    public partial class _Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                DirectoryInfo d = new DirectoryInfo(Server.MapPath(@"/TextFiles/"));
+                FileInfo[] files = d.GetFiles("*.txt");
+                DropDownListLinija.Items.Add("");
+                foreach (FileInfo f in files)
+                {
+                    string[] x = f.Name.Split(new string[] { ".txt" }, StringSplitOptions.RemoveEmptyEntries);
+                    DropDownListLinija.Items.Add(x[0]);
+                }
+            }
+        }
+
+        protected void DropDownListLinija_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            DropDownListSmer.Items.Clear();
+            string[] lines = File.ReadAllLines(Server.MapPath(@"/TextFiles/" + DropDownListLinija.SelectedValue + ".txt"));
+            foreach (string x in lines)
+            {
+                if (x.StartsWith("SMER:"))
+                {
+                    string[] smer = x.Split(new string[] { "SMER:" }, StringSplitOptions.RemoveEmptyEntries);
+                    DropDownListSmer.Items.Add(smer[0]);
+                }
+            }
+
+        }
+
+        protected void ButtonSrc_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(DropDownListLinija.SelectedValue))
+            {
+                string[] lines = File.ReadAllLines(Server.MapPath(@"/TextFiles/" + DropDownListLinija.SelectedValue + ".txt"));
+                DataTable t = new DataTable();
+                t.Columns.Add("Rbr", typeof(string));
+                t.Columns.Add("Polazak", typeof(string));
+                bool reader = false;
+                int br = 1;
+                foreach (string x in lines)
+                {
+                    if (x.StartsWith("SMER"))
+                    {
+                        reader = false;
+                        if (x.StartsWith("SMER:" + DropDownListSmer.SelectedValue))
+                        {
+                            reader = true;
+                            continue;
+                        }
+                    }
+                    if (reader)
+                    {
+                        DataRow row = t.NewRow();
+                        row[0] = br + ".";
+                        row[1] = x;
+                        br++;
+                        t.Rows.Add(row);
+                    }
+                }
+
+                GV.DataSource = t;
+                GV.DataBind();
+            }
+        }
+    }
+}
