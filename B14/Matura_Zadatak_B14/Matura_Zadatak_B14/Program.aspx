057a986cd849164c9f3fd39759b9c71f3d2bd752
nekitamolik <135228429+nekitamolik@users.noreply.github.com>
2023-06-01 16:54:12 +0200

    Add files via upload

---
+++ B14/Matura_Zadatak_B14/Matura_Zadatak_B14/Program.aspx
@@ -0,0 +1,70 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Program.aspx.cs" Inherits="Matura_Zadatak_B14._Default" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>TV Program</title>
+    <link rel="Stylesheet" href="CSS/StyleSheet.css" />
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div class="header">
+            <ul>
+                <li>Zadatak 14</li>
+                <li>TV Program</li>
+            </ul>
+        </div>
+        <div class="content">
+            <div class="search">
+                <table>
+                    <tr>
+                        <td>
+                            <label class="label">Datum:</label>
+                        </td>
+                        <td>
+                            <asp:DropDownList ID="DropDownListDatum" runat="server" Width="100px"></asp:DropDownList>
+                        </td>
+                        <td>
+                            <label class="label">Tip emisije:</label>
+                        </td>
+                        <td>
+                            <asp:DropDownList ID="DropDownListTip" runat="server" Width="200px"></asp:DropDownList>
+                        </td>
+                        <td>
+                            <asp:Button ID="ButtonSrc" runat="server" Text="Pronadji" Width="80px" OnClick="ButtonSrc_Click"/>
+                        </td>
+                    </tr>
+                </table>
+            </div>
+            <div class="GV">
+                <asp:GridView ID="GV" runat="server" OnRowDataBound="GV_RowDataBound" AutoGenerateColumns="False" >
+                    <Columns>
+                        <asp:BoundField DataField="vreme" HeaderText="Vreme" />
+                        <asp:BoundField DataField="naziv emisije" HeaderText="Naziv Emisije" />
+                        <asp:TemplateField HeaderText="Tip Emisije" >
+                            <ItemTemplate>
+                                <asp:Image ID="slika" CssClass="slika" runat="server"></asp:Image>
+                            </ItemTemplate>
+                        </asp:TemplateField>
+                    </Columns>
+                </asp:GridView>
+            </div>
+        </div>
+        <div class="footer">
+            <table>
+                <tr>
+                    <td>Elektrotehnicka skola "Nikola Tesla" Nis
+                    </td>
+                    <td>
+                        <a href="HTML Pages/Uputstvo.html">Uputstvo</a>
+                    </td>
+                    <td>
+                        <a href="HTML Pages/Kontakt.html">Kontakt</a>
+                    </td>
+                </tr>
+            </table>
+        </div>
+    </form>
+</body>
+</html>
---
+++ B14/Matura_Zadatak_B14/Matura_Zadatak_B14/Program.aspx.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.IO;
+using System.Data;
+
+namespace Matura_Zadatak_B14
+{
+    public partial class _Default : System.Web.UI.Page
+    {
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack) 
+            {
+                DropDownListDatum.Items.Clear();
+                DirectoryInfo d = new DirectoryInfo(Server.MapPath(@"/TextFiles/"));
+                FileInfo[] files = d.GetFiles("*.txt");
+                DropDownListDatum.Items.Add("");
+                foreach (FileInfo f in files)
+                {
+                    string[] x = f.Name.Split(new string[] { ".txt" }, StringSplitOptions.RemoveEmptyEntries);
+                    DropDownListDatum.Items.Add(x[0]);
+                }
+
+                DropDownListTip.Items.Clear();
+                DirectoryInfo i = new DirectoryInfo(Server.MapPath(@"\Images\"));
+                FileInfo[] images = i.GetFiles("*.png");
+                DropDownListTip.Items.Add("");
+                foreach (FileInfo img in images)
+                {
+                    string[] x = img.Name.Split(new string[] { ".png" }, StringSplitOptions.RemoveEmptyEntries);
+                    DropDownListTip.Items.Add(x[0]);
+                }
+            }
+            
+        }
+
+        protected void ButtonSrc_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(DropDownListDatum.SelectedValue))
+            {
+                DataTable t = new DataTable();
+                t.Columns.Add("Vreme", typeof(string));
+                t.Columns.Add("Naziv Emisije", typeof(string));
+                t.Columns.Add("Tip Emisije", typeof(string));
+
+                string[] lines = File.ReadAllLines(Server.MapPath(@"/TextFiles/" + DropDownListDatum.SelectedValue + ".txt"));
+                foreach (string x in lines)
+                {
+                    string[] cells = x.Split(new char[] { '|' }, StringSplitOptions.RemoveEmptyEntries);
+                    if (!string.IsNullOrEmpty(DropDownListTip.SelectedValue))
+                    {
+                        string[] type = cells[3].Split(new string[] { "\\", ".png" }, StringSplitOptions.RemoveEmptyEntries);
+                        if (type[type.Length - 1] == DropDownListTip.SelectedValue)
+                        {
+                            DataRow row = t.NewRow();
+                            row[0] = cells[0];
+                            row[1] = cells[1];
+                            row[2] = cells[3];
+                            t.Rows.Add(row);
+                        }
+                        else
+                            continue;
+                    }
+
+                    else
+                    {
+                        DataRow row = t.NewRow();
+                        row[0] = cells[0];
+                        row[1] = cells[1];
+                        row[2] = cells[3];
+                        t.Rows.Add(row);
+                    }
+                }
+
+                GV.DataSource = t;
+                GV.DataBind();
+            }
+        }
+
+        protected void GV_RowDataBound(object sender, GridViewRowEventArgs e)
+        {
+            if (e.Row.RowType == DataControlRowType.DataRow)
+            {
+                (e.Row.FindControl("slika") as Image).ImageUrl = (e.Row.DataItem as DataRowView)[2].ToString();
+            }
+        }
+
+       
+    }
+}
